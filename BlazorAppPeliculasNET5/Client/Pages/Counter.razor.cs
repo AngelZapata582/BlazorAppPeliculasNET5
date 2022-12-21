aa18dc2b985bcd4c09c2880fef514ad7f7470a25
AngelZapata582 <angelzapata582@gmail.com>
2022-12-21 17:05:36 -0600

    "pagians de gestion acores, generos, peliculas (routeo)"

--- BlazorAppPeliculasNET5/Client/Pages/Counter.razor.cs
+++ BlazorAppPeliculasNET5/Client/Pages/Counter.razor.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorAppPeliculasNET5.Client.Repositorios;
+using MathNet.Numerics.Statistics;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
@@ -9,9 +10,15 @@ namespace BlazorAppPeliculasNET5.Client.Pages
 	public partial class Counter
 	{
 		private int currentCount = 0;
+		[Inject] public IJSRuntime js { get; set; } = null!;
 
-		public  void IncrementCount()
+		public  async void IncrementCount()
 		{
+			var arreglo = new double[] { 1,2,3,4,5 };
+			var max = arreglo.Maximum();
+			var min = arreglo.Minimum();
+
+			await js.InvokeVoidAsync("alert", $"El max es {max} y el min es {min}");
 			currentCount++;
 		}
 
