31d39e56a8db2f612993856931ceaae0814a3591
AngelZapata582 <angelzapata582@gmail.com>
2022-12-22 17:53:07 -0600

    "agregado la creacion de controlador para actoes; creacion de actores;"

---
+++ BlazorAppPeliculasNET5/Server/Controllers/ActorController.cs
@@ -0,0 +1,26 @@
+﻿using BlazorAppPeliculasNET5.Shared.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BlazorAppPeliculasNET5.Server.Controllers
+{
+    [Route("api/actores")]
+    [ApiController]
+    public class ActorController:ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ActorController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Actor actor)
+        {
+            context.Add(actor);
+            await context.SaveChangesAsync();
+            return Ok(actor);
+        }
+    }
+}
