351520d31bf982281b7a915e881a8dd10f9b7873
AngelZapata582 <angelzapata582@gmail.com>
2023-01-04 13:31:35 -0600

    "deslogeo automatico despues de 30 min de inactividad;"

--- BlazorAppPeliculasNET5/Client/wwwroot/js/JavaScript.js
+++ BlazorAppPeliculasNET5/Client/wwwroot/js/JavaScript.js
@@ -8,3 +8,19 @@
 function pruebaNetInstancia(dotnetHelper) {
     dotnetHelper.invokeMethodAsync("IncrementCount");
 }
+
+function timerInactivo(dotnetHelper) {
+    var timer;
+    //reinicia el timer al mover el mouse o al presionar una tecla
+    document.onmousemove = resetTimer;
+    document.onkeypress = resetTimer;
+
+    function resetTimer() {
+        clearTimeout(timer);
+        timer = setTimeout(logout, 1000 * 60 * 30);
+    }
+
+    function logout() {
+        dotnetHelper.invokeMethodAsync("Logout");
+    }
+}
